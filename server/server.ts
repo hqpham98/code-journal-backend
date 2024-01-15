fe0af26047cbe8c973980a6d09fcd25bf7b11232
Henry Pham <hqpham98@gmail.com>
2024-01-15 19:22:38 +0000

    Created get and post methods

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import pg from 'pg';
 import express from 'express';
 import { ClientError, errorMiddleware } from './lib/index.js';
+import { nextTick } from 'process';
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -13,17 +14,59 @@ const db = new pg.Pool({
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/api/entries/', async (req, res) => {
   try {
     const sql = `
     SELECT * from "entries"`;
     const result = await db.query(sql);
-    res.send(result.rows);
+    res.json(result.rows);
   } catch (err) {
     console.log(err);
   }
 });
 
+app.get('/api/entries/:entryId', async (req, res) => {
+  try {
+    const sql = `
+    SELECT * from "entries"
+    WHERE "entryId" = $1
+    `;
+    const params = [req.params.entryId];
+    const result = await db.query(sql, params);
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post('/api/entries/', async (req, res, next) => {
+  try {
+    if (
+      !(
+        Object.keys(req.body).length === 3 &&
+        'title' in req.body &&
+        'notes' in req.body &&
+        'photoUrl' in req.body
+      )
+    ) {
+      throw Error('Error 400, Bad Request');
+    }
+    const sql = `
+    INSERT INTO "entries" ("title", "notes", "photoUrl")
+    VALUES ($1, $2, $3)
+    RETURNING *
+    `;
+
+    const { title, notes, photoUrl } = req.body;
+    const params = [title, notes, photoUrl];
+    const result = await db.query(sql, params);
+    res.json(result.rows[0]);
+  } catch (err) {
+    next(err);
+  }
+});
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
 });
